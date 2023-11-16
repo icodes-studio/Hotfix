c8fd46546f5f1528ad8495e1bd76f97cfac7416d
icoder <icodes.studio@gmail.com>
2023-11-16 15:25:01 +0900

    update settings

--- Hotfix.Runtime/ILRuntime.Test.Console/Program.cs
+++ Hotfix.Runtime/ILRuntime.Test.Console/Program.cs
@@ -11,63 +11,66 @@ namespace ILRuntime.Test.Console
         {
             if (args.Length < 2)
             {
-                System.Console.WriteLine("Usage: ILRuntime.Test.Console path useJIT[true|false]");
+                System.Console.WriteLine("Usage: mode[gen|test] <path> useJIT[true|false]");
                 return -1;
             }
 
-            //var domain = new AppDomain();
-            //using (FileStream fs = new FileStream(args[0], FileMode.Open, FileAccess.Read))
-            //{
-            //    domain.LoadAssembly(fs);
-
-            //    ILRuntimeHelper.Initialize(domain);
-            //    BindingCodeGenerator.GenerateBindingCode(domain, "..\\..\\..\\..\\ILRuntime.Test\\Generated");
-            //}
-
-            //System.Console.WriteLine("Done");
-
-            //return 0;
-
-            string path = args[0];
-            bool useJIT = args[1].ToLower() == "true";
-            var session = new TestSession();
-            session.Load(path, useJIT);
-
-            int ignoreCount = 0;
-            int todoCount = 0;
-            var failedTests = new List<TestResultInfo>();
-
-            foreach (var test in session.TestUnits)
+            if (args[0] == "gen")
             {
-                test.Run(true);
-                var result = test.CheckResult();
-                if (result.Result == TestResult.Failed)
+                var domain = new AppDomain();
+                using (FileStream fs = new FileStream(args[1], FileMode.Open, FileAccess.Read))
                 {
-                    if (result.HasToDo)
-                        todoCount++;
-                    else
-                        failedTests.Add(result);
-                }
-                else if (result.Result == TestResult.Ignored)
-                {
-                    ignoreCount++;
+                    domain.LoadAssembly(fs);
+
+                    ILRuntimeHelper.Initialize(domain);
+                    BindingCodeGenerator.GenerateBindingCode(domain, "..\\..\\..\\..\\ILRuntime.Test\\Generated");
                 }
 
-                System.Console.WriteLine(result.Message);
-                System.Console.WriteLine("-------------------------------");
-            }
+                System.Console.WriteLine("Generation done");
 
-            System.Console.WriteLine("-------------------------------");
-            System.Console.WriteLine($"{failedTests.Count} tests failed");
-            foreach (var failed in failedTests)
+                return 0;
+            }
+            else
             {
-                System.Console.WriteLine($"Test Name:{failed.TestName}, Message:{failed.Message}");
-                System.Console.WriteLine("-------------------------------");
-            }
-            System.Console.WriteLine($"Ran {session.TestUnits.Count} tests, {failedTests.Count} failded, {ignoreCount} ignored, {todoCount} todos");
-            session.Dispose();
+                var session = new TestSession();
+                session.Load(args[1], args[2].ToLower() == "true");
 
-            return failedTests.Count <= 0 ? 0 : -1;
+                int ignoreCount = 0;
+                int todoCount = 0;
+                var failedTests = new List<TestResultInfo>();
+
+                foreach (var test in session.TestUnits)
+                {
+                    test.Run(true);
+                    var result = test.CheckResult();
+                    if (result.Result == TestResult.Failed)
+                    {
+                        if (result.HasToDo)
+                            todoCount++;
+                        else
+                            failedTests.Add(result);
+                    }
+                    else if (result.Result == TestResult.Ignored)
+                    {
+                        ignoreCount++;
+                    }
+
+                    System.Console.WriteLine(result.Message);
+                    System.Console.WriteLine("-------------------------------");
+                }
+
+                System.Console.WriteLine("-------------------------------");
+                System.Console.WriteLine($"{failedTests.Count} tests failed");
+                foreach (var failed in failedTests)
+                {
+                    System.Console.WriteLine($"Test Name:{failed.TestName}, Message:{failed.Message}");
+                    System.Console.WriteLine("-------------------------------");
+                }
+                System.Console.WriteLine($"Ran {session.TestUnits.Count} tests, {failedTests.Count} failded, {ignoreCount} ignored, {todoCount} todos");
+                session.Dispose();
+
+                return failedTests.Count <= 0 ? 0 : -1;
+            }
         }
     }
 }
